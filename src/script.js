886fc0c4f76423dca9f06c1654f939a090cbd630
Gustavo-caetano <gustavocaetno@gmail.com>
2023-08-30 10:39:55 -0300

    mudanca de servidor

--- src/script.js
+++ src/script.js
@@ -1,113 +1,35 @@
-class DynamicChart {
-    constructor(chartElementId, maxDataPoints = 10) {
-        this.ctx = document.getElementById(chartElementId).getContext('2d');
-        this.chart = new Chart(this.ctx, {
-            type: 'line',
-            data: {
-                labels: [],
-                datasets: [{
-                    label: 'Dynamic Data',
-                    data: [],
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    fill: false
-                }]
-            },
-            options: {
-                scales: {
-                    x: {
-                        type: 'linear',
-                        position: 'bottom'
-                    },
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-        this.counter = 0;
-        this.maxDataPoints = maxDataPoints;
-    }
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-    updateChart(newValue) {
-        // Adicionar o valor ao gráfico
-        this.chart.data.labels.push(this.counter++);
-        this.chart.data.datasets[0].data.push(newValue);
+// Rota para servir o arquivo HTML
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-        // Atualizar o gráfico
-        this.chart.update();
-    }
+// Manipulador de eventos de conexão
+io.on('connection', (socket) => {
+  console.log('Um usuário se conectou');
 
-    stopUpdating() {
-        clearInterval(this.updateInterval);
-    }
+  // Manipulador para o evento personalizado "chat message"
+  socket.on('chat message', (msg) => {
+    console.log('Mensagem recebida: ' + msg);
 
+    // Enviar a mensagem para todos os clientes, incluindo o remetente
+    io.emit('chat message', msg);
+  });
 
-}
+  // Manipulador de eventos de desconexão
+  socket.on('disconnect', () => {
+    console.log('Um usuário se desconectou');
+  });
+});
 
-class MQTTClient {
-    constructor(wsbroker, wsport, username, password) {
-        this.client = new Paho.MQTT.Client(wsbroker, wsport, "/ws", "js" + parseInt(Math.random() * 100, 10));
-        this.username = username;
-        this.password = password;
-
-        this.client.onConnectionLost = this.onConnectionLost.bind(this);
-        this.client.onMessageArrived = this.onMessageArrived.bind(this);
-        this.client.onConnected = () => this.onConnected(); // Usar função de flecha
-
-        this.maxvalue = 0;
-
-        const options = {
-            onSuccess: () => this.onConnected(), // Usar função de flecha
-            onFailure: this.onFailure.bind(this),
-            userName: this.username,
-            password: this.password
-        };
-        
-        this.chart = new DynamicChart('myChart');
-
-        this.client.connect(options);
-    }
-
-    maxValue(newNumber) {
-        if(newNumber > this.maxvalue) {
-            this.maxvalue = newNumber
-            document.getElementById('maxValue').textContent = this.maxvalue;
-            console.log("maximo encontrado")
-        }
-    }
-
-    onConnectionLost(responseObject) {
-        console.error("CONNECTION LOST - " + responseObject.errorMessage);
-        setTimeout(() => {
-            this.connect();
-        }, 500);
-    }
-
-    onMessageArrived(message) {
-        //console.log("RECEIVE ON " + message.destinationName + " PAYLOAD " + message.payloadString);
-        this.printFirst(message.payloadString);
-    }
-
-    onConnected() {
-        console.log("Conectado ao broker MQTT");
-        this.client.subscribe("topic/esp32");
-    }
-
-    onFailure(message) {
-        console.error("Falha na conexão: " + message.errorMessage);
-    }
-
-    printFirst(message) {
-        this.maxValue(Number(message))
-        console.log("Mensagem recebida: " + message);
-        this.chart.updateChart(Number(message))
-    }
-}
-
-const wsbroker = "192.168.0.150";
-const wsport = 15675;
-const username = "gustavo";
-const password = "3141516";
-
-const client = new MQTTClient(wsbroker, wsport, username, password);
+// Iniciar o servidor na porta 3000
+server.listen(3000, () => {
+  console.log('Servidor escutando na porta 3000');
+});
