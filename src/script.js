ff61c68733009dc542ea81c118253b639a86520d
Gustavo-caetano <gustavocaetno@gmail.com>
2023-09-04 11:40:57 -0300

    teste de conexao usando apenas protocolo http

--- src/script.js
+++ src/script.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const mqtt = require('mqtt'); // Use a biblioteca MQTT
+const mqtt = require('mqtt');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Rota para servir o arquivo HTML
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/:peso',(req, res)=> {
+  const Peso = req.params.peso;
+
+  io.emit('chat message',Peso.toString())
+})
+
 io.on('connection', (socket) => {
   console.log('Um usuário se conectou');
 
-  // Conectar-se ao servidor MQTT com nome de usuário e senha
   const client = mqtt.connect('mqtt://100.68.13.36', {
     username: 'gustavo',
     password: '3141516'
@@ -25,17 +29,13 @@ io.on('connection', (socket) => {
     console.log('Conexão MQTT estabelecida com sucesso');
   });
 
-  // Manipulador para lidar com erros de conexão MQTT
   client.on('error', (error) => {
     console.error('Erro de conexão MQTT:', error.message);
     io.emit('chat message', 'Erro de conexão MQTT: ' + error.message);
-    // Você pode tomar medidas adicionais aqui, como tentar reconectar ou notificar o usuário
   });
   
-  // Subscrever-se a um tópico MQTT
   client.subscribe('esp32_data');
 
-  // Manipulador para receber mensagens do MQTT e enviá-las para o cliente via Socket.io
   client.on('message', (topic, message) => {
     const payload = message.toString();
     console.log('Mensagem recebida do MQTT:', payload);
@@ -44,12 +44,11 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('Um usuário se desconectou');
-    // Desconectar-se do servidor MQTT quando um cliente se desconectar
+
     client.end();
   });
 });
 
-// Iniciar o servidor na porta 3000
 server.listen(3000, () => {
   console.log('Servidor escutando na porta 3000');
 });
